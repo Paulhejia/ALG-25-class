4d1b98401df564bedd87e77bf99e399b951d7d45
hejiahui <1016398426@qq.com>
2021-06-07 21:05:50 +0800

    week09

---
+++ 刷题集/83. 删除排序链表中的重复元素.js
@@ -0,0 +1,32 @@
+var deleteDuplicates = function(head) {
+    if (!head) {
+        return head;
+    }
+
+    let cur = head;
+    while (cur.next) {
+        if (cur.val === cur.next.val) {
+            cur.next = cur.next.next;
+        } else {
+            cur = cur.next;
+        }
+    }
+    return head;
+};
+
+var deleteDuplicates = function(head) {
+    if(!head) {
+        return head;
+    }
+    
+    let cur = head;
+    while(cur.next) {
+        if (cur.val === cur.next.val) {
+            cur.next = cur.next.next;
+        }
+        else {
+            cur = cur.next;
+        }
+    }
+    return head;
+}
